87e73f397337e07738757b420a5054ccc692c516
Encore Chow <encorechow1992@gmail.com>
2016-08-13 12:13:42 -0700

    Refactored functional test by using help function to check input text

--- functional_tests.py
+++ functional_tests.py
@@ -3,6 +3,11 @@ import unittest
 from selenium.webdriver.common.keys import Keys
 import time
 class NewVisitorTest(unittest.TestCase):
+    def check_for_row_in_list_table(self, row_text):
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertIn(row_text, [row.text for row in rows])
+
 
     def setUp(self):
         self.browser = webdriver.Chrome()
@@ -34,10 +39,14 @@ class NewVisitorTest(unittest.TestCase):
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy peacock feathers" as an item in a to-do list table"
         inputbox.send_keys(Keys.ENTER)
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')
-        self.assertIn('1: Buy peacock feathers', [row.text for row in rows])
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        inputbox.send_keys('Use peacock feathers to make a fly')
+        inputbox.send_keys(Keys.ENTER)
+
+        self.check_for_row_in_list_table('1: Buy peacock feathers')
+        self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
 
 
         # There is still a text box inviting her to add another item. She
