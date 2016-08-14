4379e9bc85aeb6f520da6f38e60a808a8dbab92e
Encore Chow <encorechow1992@gmail.com>
2016-08-14 12:04:41 -0700

    New URL, view and template to display lists

--- lists/tests.py
+++ lists/tests.py
@@ -6,6 +6,22 @@ from django.template.loader import render_to_string
 from lists.models import Item
 # Create your tests here.
 
+
+class ListViewTest(TestCase):
+    def test_displays_all_items(self):
+        Item.objects.create(text='itemy 1')
+        Item.objects.create(text='itemy 2')
+
+
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+
+        self.assertContains(response, text='itemy 1')
+        self.assertContains(response, text='itemy 2')
+
+    def test_uses_list_template(self):
+        response = self.client.get('/lists/the-only-list-in-the-world/')
+        self.assertTemplateUsed(response, 'list.html')
+
 class ItemModelTest(TestCase):
     def test_saving_and_retrieving_items(self):
         first_item = Item()
@@ -62,7 +78,7 @@ class HomePageTest(TestCase):
         response = home_page(request)
 
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response['location'], '/')
+        self.assertEqual(response['location'], '/lists/the-only-list-in-the-world/')
 
 
     def test_home_page_only_save_items_when_necessary(self):
@@ -70,12 +86,12 @@ class HomePageTest(TestCase):
         home_page(request)
         self.assertEqual(Item.objects.count(), 0)
 
-    def test_home_page_displays_all_list_items(self):
-        Item.objects.create(text='itemy 1')
-        Item.objects.create(text='itemy 2')
-
-        request = HttpRequest()
-        response = home_page(request)
-
-        self.assertIn('itemy 1', response.content.decode())
-        self.assertIn('itemy 2', response.content.decode())
+    # def test_home_page_displays_all_list_items(self):
+    #     Item.objects.create(text='itemy 1')
+    #     Item.objects.create(text='itemy 2')
+    #
+    #     request = HttpRequest()
+    #     response = home_page(request)
+    #
+    #     self.assertContains(response, text='itemy 1')
+    #     self.assertContains(response, text='itemy 2')
--- lists/views.py
+++ lists/views.py
@@ -7,9 +7,14 @@ def home_page(request):
     # Then it builds a HttpResponse based on the content of the templates
     if request.method == 'POST':
         Item.objects.create(text=request.POST['item_text'])
-        return redirect('/')
+        return redirect('/lists/the-only-list-in-the-world/')
     # item = Item()
     # item.text = request.POST.get('item_text', '')
     # item.save()
+    #items = Item.objects.all()
+    return render(request, 'home.html')
+
+
+def view_list(request):
     items = Item.objects.all()
-    return render(request, 'home.html',{'items': items})
+    return render(request, 'list.html',{'items': items})
