2a3e019c4a5d0fe4372b38747bbe6445e205be45
Encore Chow <encorechow1992@gmail.com>
2016-08-14 12:23:30 -0700

    Refactored home_page to be separate with add new item page(new_list)

--- lists/views.py
+++ lists/views.py
@@ -5,9 +5,9 @@ from lists.models import Item
 def home_page(request):
     # render will seach for home.html from templates directory in any apps directory
     # Then it builds a HttpResponse based on the content of the templates
-    if request.method == 'POST':
-        Item.objects.create(text=request.POST['item_text'])
-        return redirect('/lists/the-only-list-in-the-world/')
+    # if request.method == 'POST':
+    #     Item.objects.create(text=request.POST['item_text'])
+    #     return redirect('/lists/the-only-list-in-the-world/')
     # item = Item()
     # item.text = request.POST.get('item_text', '')
     # item.save()
@@ -18,3 +18,7 @@ def home_page(request):
 def view_list(request):
     items = Item.objects.all()
     return render(request, 'list.html',{'items': items})
+
+def new_list(request):
+    Item.objects.create(text=request.POST['item_text'])
+    return redirect('/lists/the-only-list-in-the-world/')
