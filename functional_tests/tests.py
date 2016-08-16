9884189e2ed89b7d64c5865a4bc4ba533f1ae8e7
Encore Chow <encorechow1992@gmail.com>
2016-08-16 12:18:02 -0700

    Hack FT runner to be able to test staging

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,12 +4,30 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 import unittest
 from selenium.webdriver.common.keys import Keys
 import time
+import sys
+
+
 class NewVisitorTest(StaticLiveServerTestCase):
     def check_for_row_in_list_table(self, row_text):
         table = self.browser.find_element_by_id('id_list_table')
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
+    @classmethod
+    def setUpClass(cls):
+
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+
+    @classmethod
+    def tearDownClass(self):
+        if self.server_url == self.live_server_url:
+            super().tearDownClass();
 
     def setUp(self):
         self.browser = webdriver.Chrome()
@@ -21,7 +39,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
         # Edith has heard about a cool new online to-do app. She goes
         # to check out its homepage
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
 
         # She notices the page title and header mention to-do lists
@@ -65,7 +83,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # Francis visits the home page.  There is no sign of Edith's
         # list
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('Buy peacock feathers', page_text)
         self.assertNotIn('make a fly', page_text)
@@ -90,7 +108,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
 
     def test_layout_and_styling(self):
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         inputbox = self.browser.find_element_by_id('id_new_item')
